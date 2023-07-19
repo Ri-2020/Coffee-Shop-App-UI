75d97d9fbbf54a84bed4963603ba924f8d7628f1
Ri-2020 <rohitgupta111abcd@gmail.com>
2023-07-19 12:30:22 +0530

    login page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -11,180 +11,184 @@ class LoginPage extends StatelessWidget {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
     return Scaffold(
-      body: Container(
-        width: width,
-        height: height,
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/images/backimage.png"),
-            fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        child: Container(
+          width: width,
+          constraints: BoxConstraints(
+            minHeight: height,
           ),
-        ),
-        child: Center(
-          child: Container(
-            width: width < 500 ? width * 0.8 : 420,
-            height: height * 0.8,
-            decoration: BoxDecoration(
-              border: Border.all(
-                width: 1,
-                color: const Color.fromARGB(255, 143, 143, 143),
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/images/backimage.png"),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Center(
+            child: Container(
+              width: width < 500 ? width * 0.8 : 420,
+              height: 600,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  width: 1,
+                  color: const Color.fromARGB(255, 143, 143, 143),
+                ),
+                borderRadius: BorderRadius.circular(20),
               ),
-              borderRadius: BorderRadius.circular(20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset("assets/images/logo.png"),
+                  const Text(
+                    "Swift",
+                    style: TextStyle(
+                      fontFamily: "Raleway",
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white,
+                      fontSize: 64,
+                    ),
+                  ),
+                  const Text(
+                    "Café",
+                    style: TextStyle(
+                      fontFamily: "Raleway",
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white,
+                      fontSize: 40,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Text(
+                    "'Latte but never late'",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w200,
+                      shadows: [
+                        Shadow(
+                          color: Colors.white,
+                          blurRadius: 20,
+                          offset: Offset(0, 0),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(
+                      horizontal: 20,
+                      vertical: 5,
+                    ),
+                    child: const TextField(
+                      style: TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey),
+                        ),
+                        hintText: "User Name",
+                        hintStyle: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 15,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(
+                      horizontal: 20,
+                      vertical: 5,
+                    ),
+                    child: const TextField(
+                      style: TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.white,
+                          ),
+                        ),
+                        hintText: "Password",
+                        hintStyle: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 15,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 50,
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Get.to(const HomePage());
+                    },
+                    child: Container(
+                      padding: EdgeInsets.all(10),
+                      width: 200,
+                      decoration: BoxDecoration(
+                          gradient: const LinearGradient(colors: [
+                            Color.fromARGB(255, 77, 27, 42),
+                            Color.fromARGB(255, 167, 116, 90),
+                          ]),
+                          borderRadius: BorderRadius.circular(25)),
+                      child: const Center(
+                        child: Text(
+                          "Login",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w300,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Get.to(const HomePage());
+                    },
+                    child: Container(
+                      padding: EdgeInsets.all(10),
+                      width: 200,
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                            width: 1,
+                            color: Colors.white,
+                          ),
+                          borderRadius: BorderRadius.circular(25)),
+                      child: const Center(
+                        child: Text(
+                          "Signup",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w200,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  const Text(
+                    "Privacy Policy",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w200,
+                    ),
+                  )
+                ],
+              ),
+            ).asGlass(
+              blurX: 10,
+              blurY: 10,
+              clipBorderRadius: BorderRadius.circular(20),
             ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset("assets/images/logo.png"),
-                const Text(
-                  "Swift",
-                  style: TextStyle(
-                    fontFamily: "Raleway",
-                    fontWeight: FontWeight.w400,
-                    color: Colors.white,
-                    fontSize: 64,
-                  ),
-                ),
-                const Text(
-                  "Café",
-                  style: TextStyle(
-                    fontFamily: "Raleway",
-                    fontWeight: FontWeight.w400,
-                    color: Colors.white,
-                    fontSize: 40,
-                  ),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                const Text(
-                  "'Latte but never late'",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.w200,
-                    shadows: [
-                      Shadow(
-                        color: Colors.white,
-                        blurRadius: 20,
-                        offset: Offset(0, 0),
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Container(
-                  margin: const EdgeInsets.symmetric(
-                    horizontal: 20,
-                    vertical: 5,
-                  ),
-                  child: const TextField(
-                    style: TextStyle(color: Colors.white),
-                    decoration: InputDecoration(
-                      enabledBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.grey),
-                      ),
-                      hintText: "User Name",
-                      hintStyle: TextStyle(
-                        color: Colors.grey,
-                        fontSize: 15,
-                      ),
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.symmetric(
-                    horizontal: 20,
-                    vertical: 5,
-                  ),
-                  child: const TextField(
-                    style: TextStyle(color: Colors.white),
-                    decoration: InputDecoration(
-                      enabledBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Colors.white,
-                        ),
-                      ),
-                      hintText: "Password",
-                      hintStyle: TextStyle(
-                        color: Colors.grey,
-                        fontSize: 15,
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 50,
-                ),
-                TextButton(
-                  onPressed: () {
-                    Get.to(const HomePage());
-                  },
-                  child: Container(
-                    padding: EdgeInsets.all(10),
-                    width: 200,
-                    decoration: BoxDecoration(
-                        gradient: const LinearGradient(colors: [
-                          Color.fromARGB(255, 77, 27, 42),
-                          Color.fromARGB(255, 167, 116, 90),
-                        ]),
-                        borderRadius: BorderRadius.circular(25)),
-                    child: const Center(
-                      child: Text(
-                        "Login",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w300,
-                          fontSize: 20,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                TextButton(
-                  onPressed: () {
-                    Get.to(const HomePage());
-                  },
-                  child: Container(
-                    padding: EdgeInsets.all(10),
-                    width: 200,
-                    decoration: BoxDecoration(
-                        border: Border.all(
-                          width: 1,
-                          color: Colors.white,
-                        ),
-                        borderRadius: BorderRadius.circular(25)),
-                    child: const Center(
-                      child: Text(
-                        "Signup",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w200,
-                          fontSize: 20,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                const Text(
-                  "Privacy Policy",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.w200,
-                  ),
-                )
-              ],
-            ),
-          ).asGlass(
-            blurX: 10,
-            blurY: 10,
-            clipBorderRadius: BorderRadius.circular(20),
           ),
         ),
       ),
